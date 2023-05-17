6eebf8dfc5419c9173b0e8807a778cf6e9b14938
P-B Monaco <imfonky@gmail.com>
2023-05-17 12:38:31 +0200

    testing with simplier queue

---
+++ tspcc/lifo.hpp
@@ -0,0 +1,66 @@
+
+#ifndef _lifo_hpp
+#define _lifo_hpp
+
+#include <atomic>
+
+template <typename T>
+class AtomicLifo
+{
+private:
+    T **data;
+    int top;
+    int size;
+    std::atomic_flag _lock = ATOMIC_FLAG_INIT;
+public:
+    AtomicLifo(int size)
+    {
+        data = new T*[size];
+        top = -1;
+        this->size = size;
+    }
+
+    ~AtomicLifo()
+    {
+        delete data;
+    }
+
+    bool push(T* item)
+    {
+        int t = top;
+
+        if(t >= size){
+            return false;
+        }
+        int newt = t+1;
+        if(!__atomic_compare_exchange(&top, &t, &newt, 0, __ATOMIC_RELAXED, __ATOMIC_RELAXED)){
+            return false;
+        }
+        data[newt] = item;
+        return true;
+    }
+
+
+    T* pop()
+    {
+        int t = top;
+        T* item;
+
+        if(t <= 0){
+            return nullptr;
+        }
+        int newt = t-1;
+        if(!__atomic_compare_exchange(&top, &t, &newt, 0, __ATOMIC_RELAXED, __ATOMIC_RELAXED)){
+            return nullptr;
+        }
+        item = data[newt];
+
+        return item;
+    }
+
+    int get_size(){
+        return top+1;
+    }
+};
+
+#endif //_lifo_hpp
